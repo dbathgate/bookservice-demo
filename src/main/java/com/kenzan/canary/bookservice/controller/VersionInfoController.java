998c09196d932147539ced8dd2c1e6c699ad76da
Darren Bathgate <dbathgate@kenzan.com>
2018-06-28 10:36:08 -0400

    updating version controller

--- src/main/java/com/kenzan/canary/bookservice/controller/VersionInfoController.java
+++ src/main/java/com/kenzan/canary/bookservice/controller/VersionInfoController.java
@@ -4,6 +4,8 @@ import javax.annotation.PostConstruct;
 
 import com.kenzan.canary.bookservice.dto.VersionInfoDto;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +20,13 @@ public class VersionInfoController {
     @Value("${app.version:1.0}")
     private String appVersion;
 
-    private Counter getVersionCounter;
-
-    @PostConstruct
-    public void init() {
-        this.getVersionCounter = Counter.build().name("getVersionCounter").help("Get Version Info").register();
-    }
+    @Autowired
+    @Qualifier("getHttpRequestsTotalCounter")
+    private Counter httpRequestTotalCounter;
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<VersionInfoDto> getVersionInfo() {
-        getVersionCounter.inc();;
+        httpRequestTotalCounter.labels("getVersion", "GET", "200").inc();
 
         VersionInfoDto versionInfoDto = new VersionInfoDto(appVersion);
         return ResponseEntity.ok(versionInfoDto);
